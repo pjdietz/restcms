f66c1948506c33922f36cd251be93ca3cb46f736
PJ Dietz <pjdietz@gmail.com>
2012-09-23 18:45:07 -0400

    WellRESTed Router now returns the request, and Handlers are intermediaries only.

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,9 +3,17 @@
 require_once('restcms/MainRouter.inc.php');
 
 $router = new MainRouter();
+$response = $router->getResponse();
+$response->respond();
+
+/*
 $handler = $router->getRequestHandler();
 $response = $handler->getResponse();
 $response->respond();
+*/
+
 exit;
+
+// TODO: Default response
 
 ?>
