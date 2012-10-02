b86e772e663f97df38330add34b93ca67e1a9315
PJ Dietz <pjdietz@gmail.com>
2012-10-01 20:16:50 -0400

    Some updates to WellRESTed.

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,15 +5,6 @@ require_once('restcms/MainRouter.inc.php');
 $router = new MainRouter();
 $response = $router->getResponse();
 $response->respond();
-
-/*
-$handler = $router->getRequestHandler();
-$response = $handler->getResponse();
-$response->respond();
-*/
-
 exit;
-
-// TODO: Default response
 
 ?>
