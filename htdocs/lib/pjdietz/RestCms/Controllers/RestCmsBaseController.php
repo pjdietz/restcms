5bc3f21f96764dfb6cb720d8b3fab20c3f7d273c
PJ Dietz <pjdietz@gmail.com>
2013-06-08 13:30:35 -0400

    Remove data member from RestCmsBaseController. Close #2

--- htdocs/lib/pjdietz/RestCms/Controllers/RestCmsBaseController.php
+++ htdocs/lib/pjdietz/RestCms/Controllers/RestCmsBaseController.php
@@ -14,36 +14,6 @@ use PDO;
 abstract class RestCmsBaseController
 {
 
-    /**
-     * The instance's main data store.
-     *
-     * @var mixed
-     */
-    protected $data;
-
-    // -------------------------------------------------------------------------
-    // Accessors
-
-    /**
-     * @param string $name
-     * @return array|string
-     * @throws \Exception
-     */
-    public function __get($name)
-    {
-        switch ($name) {
-            case 'data':
-                return $this->getData();
-            default:
-                throw new \Exception('Property ' . $name . ' does not exist.');
-        }
-    }
-
-    public function getData()
-    {
-        return $this->data;
-    }
-
     // -------------------------------------------------------------------------
     // Temp Table Helpers
 
