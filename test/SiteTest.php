3e52ad3f1216928dd826e28ef152857ac0915664
PJ Dietz <pjdietz@gmail.com>
2014-07-31 20:52:34 -0400

    Reorganizer and added PHPUnit

---
+++ test/SiteTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace pjdietz\RestCms\Test;
+
+class SiteTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFakeTest()
+    {
+        $this->assertTrue(true);
+    }
+}
